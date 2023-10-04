ef6857b8174168f781095187ad3b248cdaf3b058
javier_garcia <javi7garcia1820@gmail.com>
2023-10-04 18:03:02 +0200

    Send directory for work in home

---
+++ cell0/ex04/argv.sh
@@ -0,0 +1,8 @@
+ if [ $# -eq 0 ];then
+ echo "no se proporcionan argumentos"
+ elif [ $# -gt 3 ] ;then
+ echo "$1"
+ echo "$2"
+ echo "$3"
+ fi
+
---
+++ cell0/ex05/build.sh.save
@@ -0,0 +1,9 @@
+#!/bin/bash
+for arg in "$@"
+do
+mkdir "ex$arg"
+done
+ if [[  $@ -eq "" ]]
+ then
+ echo "no arguments"
+fi
